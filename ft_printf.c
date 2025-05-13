2bd122f880914cced89f407fbb8b4e4a7590eef4
mxmklvrv <maximkleverov@icloud.com>
2025-05-13 13:18:08 +0300

    base for printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,36 +6,47 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:25:04 by mklevero          #+#    #+#             */
-/*   Updated: 2025/05/12 20:23:43 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/05/13 13:16:47 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-void	ft_printf(const char *format, ...)
+
+int check_format(char ch, va_list args)
+{
+	// to check the char and send it to corresponding function
+}
+
+
+
+int	ft_printf(const char *format, ...)
 {
 	va_list	args;
+	int	total;
+	int count;
 
+	if (!format)
+		return (-1);
+	total = 0; 
+	count = 0;
 	va_start(args, format);
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format == '%') // what if afther '%' there is \0 ?? 
 		{
 			format++;
-			if (*format == 'd')
-				printf("%d", va_arg(args, int));
-			else if (*format == 's')
-				printf("%s", va_arg(args, char *));
+			count= check_format(*format, &args);
 		}
 		else
-			putchar(*format);
+			count = write(1, format, 1);
+		if (count == -1)
+			return(va_end(args), -1);
+		total+= count;
 		format++;
 	}
-	va_end(args);
+	return(va_end(args), total);
 }
-int	main(void)
-{
-	ft_printf("hello %s, numm of  %d\n", "gene", 42);
-	printf(" mama4ka");
-}
+
+
