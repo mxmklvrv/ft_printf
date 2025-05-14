77a15a87d6a0b7a9e9899574a84b69a5e6a8b153
mxmklvrv <maximkleverov@icloud.com>
2025-05-14 14:46:19 +0300

    Add files via upload

--- ft_helper_num.c
+++ ft_helper_num.c
@@ -6,15 +6,16 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 19:00:05 by mklevero          #+#    #+#             */
-/*   Updated: 2025/05/13 20:09:12 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/05/14 14:45:40 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnum(int n)
+int	ft_putnum(int n) 
 {
 	int		i;
+	int		print;
 	long	dup;
 	char	res;
 
@@ -28,10 +29,32 @@ int	ft_putnum(int n)
 		dup = -dup;
 	}
 	if (dup >= 10)
-		i += ft_putnum(dup / 10);
+	{
+		print = ft_putnum(dup / 10);
+		if (print < 0)
+			return (-1);
+		i+= print;
+	}
 	res = (dup % 10) + '0';
 	if (ft_putchar(res) < 0)
 		return (-1);
 	i++;
 	return (i);
 }
+
+int ft_putnum_unsigned(unsigned int n)
+{
+	int i;
+	char res;
+
+	i = 0;
+	if (n >= 10)
+		i+= ft_putnum_unsigned(n / 10);
+	res = (n % 10) + '0';
+	if (ft_putchar(res) < 0)
+		return (-1);
+	i++;
+	return (i);
+}
+
+
